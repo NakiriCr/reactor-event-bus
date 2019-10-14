b4b5c5515d62806ec8c333c6eed4a165a83a4b40
VirtualCry <172519267@QQ.COM>
2019-10-14 09:54:25 +0800

    refact implement of de-duplication filter.

--- src/main/java/cn/gitlab/virtualcry/reactor/bus/filter/DeDuplicationFilter.java
+++ src/main/java/cn/gitlab/virtualcry/reactor/bus/filter/DeDuplicationFilter.java
@@ -2,14 +2,18 @@ package cn.gitlab.virtualcry.reactor.bus.filter;
 
 import cn.gitlab.virtualcry.reactor.bus.registry.Registration;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
- * A {@link Filter} implementation that performs no filtering, returning the {@code items} as-is.
+ * A {@link Filter} implementation that returns the {@code items} after de-duplication.
  *
  * @author VirtualCry
- *
+ * @since 3.2.2
  */
 @SuppressWarnings("unchecked")
 public final class DeDuplicationFilter extends AbstractFilter {
@@ -18,10 +22,12 @@ public final class DeDuplicationFilter extends AbstractFilter {
 	public <T> List<T> doFilter(List<T> items, Object key) {
 		return items.stream()
 				.filter(item -> item instanceof Registration)
-				.map(item -> ((Registration) item))
-				.collect(Collectors.toMap(Registration::getObject, item -> item, (v1, v2) -> v1))
-				.values().stream()
-				.map(item -> (T) item)
+				.filter(distinctByKey(item -> ((Registration) item).getObject()))
 				.collect(Collectors.toList());
+	}
+
+	private <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
+		Map<Object, Boolean> cache = new HashMap<>();
+		return t -> cache.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
 	}
 }
